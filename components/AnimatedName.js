9ce02d204b5daf1400baba0cf2604225992f82c7
Dominic Martin Cruz <dmcruz.wf@gmail.com>
2022-07-17 17:40:17 +0800

    made StickyHoverComponent reusable

--- components/AnimatedName.js
+++ components/AnimatedName.js
@@ -1,59 +1,16 @@
 import AnimatedText from './AnimatedText';
 import classes from '../styles/animatedText.module.css';
-import { useState, useRef, useEffect } from 'react';
+import { useState } from 'react';
+import StickyHoverComponent from './StickyHoverComponent';
 
 const AnimatedName = () => {
-  const [translate, setTranslate] = useState({ x: 0, y: 0 });
-  const boundsRef = useRef(null);
-  const divsArrRef = useRef(null);
-
   return (
     <div className={classes.container}>
       <div className={classes.textContainer}>
-        <div
+        <StickyHoverComponent
           id={classes.hoverLetterContainer}
-          onMouseOver={(e) => {
-            divsArrRef.current = [
-              document.getElementById(classes.d),
-              document.getElementById(classes.m),
-              document.getElementById(classes.b),
-              document.getElementById(classes.c),
-            ];
-            boundsRef.current = e.target.getBoundingClientRect();
-          }}
-          onMouseMove={(e) => {
-            if (!boundsRef.current) return;
-            const bounds = boundsRef.current;
-            const a = bounds.x + bounds.width / 2;
-            const b = bounds.y + bounds.height / 2;
-            setTranslate({
-              x: e.clientX - a,
-              y: e.clientY - b,
-            });
-
-            if (
-              e.clientX > bounds.x + bounds.width ||
-              e.clientY > bounds.y + bounds.height
-            ) {
-              divsArrRef.current.map((el) => {
-                el.style.transform = `translate(0px, 0px)`;
-              });
-            } else {
-              divsArrRef.current.map((el) => {
-                el.style.transform = `translate(${translate.x}px, ${translate.y}px)`;
-              });
-            }
-          }}
-          onMouseOut={(e) => {
-            setTranslate({
-              x: 0,
-              y: 0,
-            });
-            divsArrRef.current.map((el) => {
-              el.style.transform = `translate(0px, 0px)`;
-            });
-          }}
-        ></div>
+          divs={[classes.d, classes.m, classes.b, classes.c]}
+        />
         <div className={classes.letterD}>
           <AnimatedText name={'d'} id={classes.d} />
         </div>
