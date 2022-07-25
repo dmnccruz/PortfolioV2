cf8369857b90ea9e1ccacae6483394911a02341b
Dominic Martin Cruz <dmcruz.wf@gmail.com>
2022-07-25 23:51:51 +0800

    added Head component with animation and lighting following cursor, added Page1 component, fixed index, App, Page1, and Head css

--- src/App.js
+++ src/App.js
@@ -1,59 +1,10 @@
-import { Canvas, useFrame } from '@react-three/fiber';
-import { useLoader } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Suspense, useRef } from 'react';
-import {
-  Environment,
-  OrbitControls,
-  PresentationControls,
-} from '@react-three/drei';
-
-const Model = () => {
-  const gltf = useLoader(GLTFLoader, './loomis/scene.gltf');
-  const rotateMesh = useRef();
-
-  useFrame(({ clock }) => {
-    const rotateSpeed = Math.sin(clock.getElapsedTime()) / 2;
-    // const rotateSpeed = clock.getElapsedTime() / 10;
-    rotateMesh.current.rotation.y = rotateSpeed;
-  });
-
-  return (
-    <mesh ref={rotateMesh}>
-      <primitive object={gltf.scene} scale={0.5} />
-    </mesh>
-  );
-};
+import Page1 from './components/Page1';
+import './styles/App.css';
 
 const App = () => {
   return (
-    <div
-      className='App'
-      style={{
-        background: 'white',
-        height: '100%',
-        width: '100%',
-      }}
-    >
-      <Canvas>
-        <Suspense fallback={null}>
-          <PresentationControls
-            global={false} // Spin globally or by dragging the model
-            cursor={true} // Whether to toggle cursor style on drag
-            snap={false} // Snap-back to center (can also be a spring config)
-            speed={1} // Speed factor
-            zoom={1} // Zoom factor when half the polar-max is reached
-            rotation={[0, 0, 0]} // Default rotation
-            polar={[Math.PI / 10, Math.PI / 10]} // Vertical limits
-            azimuth={[-Infinity, Infinity]} // Horizontal limits
-            config={{ mass: 1, tension: 1000, friction: 26 }} // Spring config
-          >
-            <Model />
-            {/* <OrbitControls /> */}
-            <Environment preset='sunset' background={false} />
-          </PresentationControls>
-        </Suspense>
-      </Canvas>
+    <div className='App'>
+      <Page1 />
     </div>
   );
 };
---
+++ src/components/Page1.js
@@ -0,0 +1,12 @@
+import Head from './Head';
+import '../styles/Page1.css';
+
+const Page1 = () => {
+  return (
+    <div className={'Page1'}>
+      <Head />
+    </div>
+  );
+};
+
+export default Page1;
