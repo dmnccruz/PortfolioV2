50bcc4b4ee81db74db4b7f4257aa628113e5d71b
Dominic Martin Cruz <dmcruz.wf@gmail.com>
2022-07-29 00:55:12 +0800

    added text, removed unused lines and files

---
+++ src/components/HeadModel.js
@@ -0,0 +1,118 @@
+import * as THREE from 'three';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Suspense, useRef } from 'react';
+import { Html } from '@react-three/drei';
+import '../styles/HeadModel.css';
+
+function Model(props) {
+  const group = useRef();
+  const gltf = useLoader(GLTFLoader, './davidwithgoldbottom.gltf');
+
+  useFrame(({ pointer }) => {
+    group.current.rotation.y = THREE.MathUtils.lerp(
+      group.current.rotation.y,
+      pointer.x * (Math.PI / 5),
+      0.01
+    );
+    group.current.rotation.x = THREE.MathUtils.lerp(
+      group.current.rotation.x,
+      -pointer.y * (Math.PI / 5),
+      0.01
+    );
+  });
+  return (
+    <group ref={group} {...props}>
+      <mesh castShadow receiveShadow scale={[1, 1, 1]} dispose={null}>
+        <primitive object={gltf.scene} scale={2} />
+      </mesh>
+      <Lights />
+    </group>
+  );
+}
+
+function Lights() {
+  const groupL = useRef();
+  const groupR = useRef();
+  const front = useRef();
+  useFrame(({ pointer }) => {
+    groupL.current.rotation.y = THREE.MathUtils.lerp(
+      groupL.current.rotation.y,
+      -pointer.x * (Math.PI / 2),
+      0.1
+    );
+    groupR.current.rotation.y = THREE.MathUtils.lerp(
+      groupR.current.rotation.y,
+      pointer.x * (Math.PI / 2),
+      0.1
+    );
+    front.current.position.x = THREE.MathUtils.lerp(
+      front.current.position.x,
+      pointer.x * 12,
+      0.05
+    );
+    front.current.position.y = THREE.MathUtils.lerp(
+      front.current.position.y,
+      7 + pointer.y * 4,
+      0.05
+    );
+  });
+
+  return (
+    <>
+      <group ref={groupL}>
+        <pointLight position={[0, 7, -15]} distance={15} intensity={2} />
+      </group>
+      <group ref={groupR}>
+        <pointLight position={[0, 7, -15]} distance={15} intensity={2} />
+      </group>
+      <pointLight
+        ref={front}
+        position={[50, -50, 10]}
+        distance={15}
+        intensity={10}
+        decay={1}
+        shadow={true}
+      />
+    </>
+  );
+}
+
+function Zoom() {
+  useFrame((state) => {
+    state.camera.position.lerp({ x: 0, y: 0, z: 12 }, 0.005);
+    state.camera.lookAt(0, 0, 0);
+  });
+}
+
+const HeadModel = () => {
+  return (
+    <div
+      className='HeadModel'
+      style={{
+        // background:
+        //   'radial-gradient(circle, rgba(10,10,10,1) 0%, rgba(0,0,0,1) 35%)',
+        background: 'black',
+      }}
+    >
+      <Canvas shadows camera={{ position: [0, 1.5, 14], fov: 75 }}>
+        <fog attach='fog' args={['black', 0, 20]} />
+        <pointLight position={[0, 50, 180]} intensity={1} />
+        <Suspense
+          fallback={
+            // TODO: ADD LOADER
+            <Html center className='loader'>
+              LOADING
+            </Html>
+          }
+        >
+          <Model position={[0, -2, 0]} rotation={[0, -0.2, 0]} />
+          <Zoom />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+};
+
+export default HeadModel;
--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,10 +1,17 @@
-import Head from './Head';
+import HeadModel from './HeadModel';
 import '../styles/Page1.css';
 
 const Page1 = () => {
   return (
     <div className={'Page1'}>
-      <Head />
+      <div className={'text'}>
+        <h1 className={'name'}>D O M I N I C M A R T I N C R U Z</h1>
+        <span>
+          __________________________________________________________________________
+        </span>
+        <h1 className={'subtext'}>s o f t w a r e • d e v e l o p e r</h1>
+      </div>
+      <HeadModel />
     </div>
   );
 };
