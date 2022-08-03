11512e70caf4fc6b98a15ae4a07dc72e8c0d08fc
Dominic Martin Cruz <dmcruz.wf@gmail.com>
2022-08-03 21:52:46 +0800

    fixed name and contact from styling

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -5,11 +5,19 @@ const Page1 = ({ id }) => {
   return (
     <div className={'Page1'} id={id}>
       <div className={'text'}>
-        <h1 className={'name'}>
-          <span> D O M I N I C </span>M A R T I N<span> B. </span>C R U Z
-        </h1>
-        <span>
-          __________________________________________________________________________
+        <div className={'name'}>
+          <h1>D&nbsp;O&nbsp;M&nbsp;I&nbsp;N&nbsp;I&nbsp;C&nbsp;</h1>
+          <h2>M&nbsp;A&nbsp;R&nbsp;T&nbsp;I&nbsp;N&nbsp;</h2>
+          <h1>B.&nbsp;</h1>
+          <h2>C&nbsp;R&nbsp;U&nbsp;Z</h2>
+        </div>
+        {/* <h1 className={'name'}>
+          <span> D&nbsp;O&nbsp;M&nbsp;I&nbsp;N&nbsp;I&nbsp;C</span>
+          M&nbsp;A&nbsp;R&nbsp;T&nbsp;I&nbsp;N<span> B. </span>
+          C&nbsp;R&nbsp;U&nbsp;Z
+        </h1> */}
+        <span style={{ zIndex: -1 }}>
+          _______________________________________________________________________________________________________________________________________
         </span>
         <h1 className={'subtext'}>
           s o f t w a r e <span> • </span> d e v e l o p e r
